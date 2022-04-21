8b5685a0fa7e3225ae169909a79c20dab304c845
kasznar <david.kasznar@benestudio.co>
2022-04-21 18:56:14 +0200

    added second app

---
+++ apps/sporty-brand-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-stripe-workshop-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'sporty-brand-web';
+}
