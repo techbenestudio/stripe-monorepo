dad43619983ec86153f255e0cb7512db8fa39c08
kasznar <david.kasznar@benestudio.co>
2022-04-21 19:02:25 +0200

    mock data

--- apps/fashion-backend/src/main.ts
+++ apps/fashion-backend/src/main.ts
@@ -7,8 +7,56 @@ import * as express from 'express';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to fashion-backend!' });
+app.use('/api/static', express.static(__dirname + '/assets'));
+
+app.get('/api/elegant', (req, res) => {
+  res.send({
+    products: [
+      {
+        id: 1,
+        name: 'fancy shoes',
+        img: '/api/static/fancy-shoes.jpg',
+        price: 10000,
+      },
+      {
+        id: 2,
+        name: 'fancy overall',
+        img: '/api/static/fancy-overall.jpg',
+        price: 30000,
+      },
+      {
+        id: 3,
+        name: 'suit',
+        img: '/api/static/suit.jpg',
+        price: 100000,
+      },
+    ],
+  });
+});
+
+app.get('/api/sporty', (req, res) => {
+  res.send({
+    products: [
+      {
+        id: 4,
+        name: 'running shoes',
+        img: '/api/static/running-shoe.jpg',
+        price: 10000,
+      },
+      {
+        id: 5,
+        name: 'sweatpants',
+        img: '/api/static/sweatpants.jpg',
+        price: 30000,
+      },
+      {
+        id: 6,
+        name: 'arctic jacket',
+        img: '/api/static/arctic-jacket.jpg',
+        price: 100000,
+      },
+    ],
+  });
 });
 
 const port = process.env.port || 3333;
